63d405f482310f47fe2a2818df325d11a381183d
JPisaBrony <jdpohl789@gmail.com>
2017-10-19 11:31:23 -0500

    + signalr to layout instead of home to be in a global context

--- Signalr/TemplateSignalrUI/src/js/components/Home.js
+++ Signalr/TemplateSignalrUI/src/js/components/Home.js
@@ -4,20 +4,6 @@ import $ from 'jquery';
 window.jQuery = $;
 
 export default class Home extends React.Component {
-    componentWillMount() {
-        var connection = $.hubConnection("http://localhost:8081/");
-        this.hub = connection.createHubProxy("examplehub");
-
-        connection.start().done(() => {
-            console.log("Signalr started");
-            this.hub.invoke("ExampleMethod", "example method invoke");
-        });
-
-        this.hub.on('examplemsg', (msg) => {
-            console.log(msg);
-        });
-    }
-
     render() {
         return (
             <div>
--- Signalr/TemplateSignalrUI/src/js/components/Layout.js
+++ Signalr/TemplateSignalrUI/src/js/components/Layout.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { connect } from 'react-redux';
 import Navigation from "./Navigation";
+import $ from 'jquery';
+window.jQuery = $;
 
 export default class Layout extends React.Component {
+    componentWillMount() {
+        var connection = $.hubConnection("http://localhost:8081/");
+        this.hub = connection.createHubProxy("examplehub");
+
+        connection.start().done(() => {
+            console.log("Signalr started");
+            this.hub.invoke("ExampleMethod", "example method invoke");
+        });
+
+        this.hub.on('examplemsg', (msg) => {
+            console.log(msg);
+        });
+    }
+
     render() {
         return (
             <div class="container-fluid">
