a72790976505ffbae9b863f7f34990891c91da34
JPisaBrony <jdpohl789@gmail.com>
2017-10-19 12:47:39 -0500

    + updated dependencies

--- Signalr/TemplateSignalrUI/src/js/reducers/index.js
+++ Signalr/TemplateSignalrUI/src/js/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import { routerReducer } from 'react-router-redux';
 import settings from "./settingsReducer";
 import example from "./exampleReducer";
 
 export default combineReducers({
     example,
-    settings,
-    router: routerReducer
+    settings
 });
