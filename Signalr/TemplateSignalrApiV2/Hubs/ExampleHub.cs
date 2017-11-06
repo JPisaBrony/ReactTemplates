ba1ee41188e382bb678b3bae141699625f548895
JPisaBrony <jdpohl789@gmail.com>
2017-11-06 14:26:00 -0600

    + updated version of signalr

---
+++ Signalr/TemplateSignalrApiV2/Hubs/ExampleHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TemplateSignalrApiV2.Hubs
+{
+    public class ExampleHub : Hub
+    {
+        public void ExampleSendAll(string msg)
+        {
+            Clients.All.InvokeAsync("examplesend", msg);
+        }
+
+        public void ExampleSendCaller(string msg)
+        {
+            Clients.Client(Context.ConnectionId).InvokeAsync("examplesend", msg);
+        }
+    }
+}
---
+++ Signalr/TemplateSignalrUIV2/webpack.config.js
@@ -0,0 +1,38 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: "./js/client.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      },
+      { 
+        test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      },
+      {
+		test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000'
+	  }
+    ]
+  },
+  output: {
+    path: __dirname + "/wwwroot/",
+    filename: "client.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
