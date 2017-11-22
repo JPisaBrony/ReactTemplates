026ac97c4fba5a3b3d7b3658c178a3b4d597f346
JPisaBrony <jdpohl789@gmail.com>
2017-11-22 11:42:36 -0600

    + bing maps template

---
+++ TemplateBingMap/src/js/client.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import "../style.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import "toastr/build/toastr.min.css";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import ExampleRoute from "./components/ExampleRoute";
+import Map from "./components/Map";
+import store from "./store";
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={hashHistory}>
+            <Route path="/" component={Layout}>
+                <IndexRoute component={Home}></IndexRoute>
+                <Route path="ExampleRoute" component={ExampleRoute}></Route>
+                <Route path="Map" component={Map}></Route>
+            </Route>
+        </Router>
+    </Provider>,
+app);
---
+++ TemplateBingMap/src/js/components/Navigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default class Navigation extends React.Component {
+    render() {
+        return (
+            <div style={{ paddingTop: 10, fontSize: 20 }}>
+                <div class="row">
+                    <div class="col-md-12">
+                        <Link to="/">Home</Link>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <Link to="ExampleRoute">Example Route</Link>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <Link to="Map">Map</Link>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
